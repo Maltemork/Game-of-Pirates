8529fb9e9d0a3e718e9d18583417fa2a3b89868f
Maltemork <maltems1996@gmail.com>
2023-03-02 16:01:45 +0100

    A LOT

--- script.js
+++ script.js
@@ -1,13 +1,61 @@
 "use strict";
-window.addEventListener("load", start);
+window.addEventListener("load", ready);
 
 // ======= Variables ======= //
 let points = 0;
 let lives = 3;
-
+let isGameRunning = false;
+let highScore = 0;
 // ======= Start ======= //
-function start() {
-  console.log("JavaScript kører!");
+function ready() {
+  document
+    .querySelector("#btn_start_game")
+    .addEventListener("click", startGame);
+  document.querySelector("#btn_restart").addEventListener("click", startGame);
+  document
+    .querySelector("#btn_go_to_start")
+    .addEventListener("click", showStartScreen);
+}
+
+function showGameScreen() {
+  // Skjul startskærm, game over og level complete
+  document.querySelector("#game_start").classList.add("hidden");
+  document.querySelector("#game_over").classList.add("hidden");
+  document.querySelector("#level_complete").classList.add("hidden");
+}
+
+function showStartScreen() {
+  // fjern hidden fra startskærm og tilføj til game over og level complete
+  document.querySelector("#game_start").classList.remove("hidden");
+  document.querySelector("#game_over").classList.add("hidden");
+  document.querySelector("#level_complete").classList.add("hidden");
+}
+
+function resetLives() {
+  // sæt lives til 3
+  lives = 3;
+  //nulstil visning af liv (hjerte vi ser)
+  document.querySelector("#rum1").classList.remove("broken_rum");
+  document.querySelector("#rum2").classList.remove("broken_rum");
+  document.querySelector("#rum3").classList.remove("broken_rum");
+  document.querySelector("#rum1").classList.add("active_rum");
+  document.querySelector("#rum2").classList.add("active_rum");
+  document.querySelector("#rum3").classList.add("active_rum");
+}
+
+function resetPoints() {
+  points = 0;
+  displayPoints();
+}
+
+function startGame() {
+  resetLives();
+  resetPoints();
+  showGameScreen();
+  startTimer();
+  isGameRunning = true;
+  console.log("Game started!");
+  document.querySelector("#game_start").classList.add("hidden");
   startAnimations();
   startClicks();
   startPositions();
@@ -21,7 +69,6 @@ function startPositions() {
   document.querySelector("#treasure3_container").classList.add("position3");
   document.querySelector("#skull_container").classList.add("position4");
   document.querySelector("#rum_container").classList.add("position5");
-  document.querySelector("#rum_container").addEventListener("animationend", rumRestart);
 }
 
 function startClicks() {
@@ -37,14 +84,36 @@ function startClicks() {
   document
     .querySelector("#skull_container")
     .addEventListener("mousedown", clickSkull);
-  document.querySelector("#rum_container").addEventListener("click", rumRestart);
+  document
+    .querySelector("#rum_container")
+    .addEventListener("mousedown", clickRum);
 }
 
 function startAnimations() {
-  document.querySelector("#treasure1_container").classList.add("jumping3");
+  //fjern animations
+  document
+    .querySelector("#treasure1_container")
+    .classList.remove("jumping1", "jumping2", "jumping3");
+  document
+    .querySelector("#treasure2_container")
+    .classList.remove("jumping1", "jumping2", "jumping3");
+  document
+    .querySelector("#treasure3_container")
+    .classList.remove("jumping1", "jumping2", "jumping3");
+  document.querySelector("#skull_container").classList.remove("jumping_skull");
+  document.querySelector("#rum_container").classList.remove("jumping_rum");
+
+  //opdater containers
+  document.querySelector("#treasure1_container").offsetWidth;
+  document.querySelector("#treasure2_container").offsetWidth;
+  document.querySelector("#treasure3_container").offsetWidth;
+  document.querySelector("#skull_container").coffsetWidth;
+  document.querySelector("#rum_container").offsetWidth;
+  //tilføj animations
+  document.querySelector("#treasure1_container").classList.add("jumping2");
   document.querySelector("#treasure2_container").classList.add("jumping3");
-  document.querySelector("#treasure3_container").classList.add("jumping3");
-  document.querySelector("#skull_container").classList.add("jumping2");
+  document.querySelector("#treasure3_container").classList.add("jumping1");
+  document.querySelector("#skull_container").classList.add("jumping_skull");
   document.querySelector("#rum_container").classList.add("jumping_rum");
 }
 
@@ -60,7 +129,7 @@ function animationEnd() {
     .addEventListener("animationend", treasureRestart);
   document
     .querySelector("#skull_container")
-    .addEventListener("animationend", treasureRestart);
+    .addEventListener("animationend", skullRestart);
   document
     .querySelector("#rum_container")
     .addEventListener("animationend", rumRestart);
@@ -68,7 +137,7 @@ function animationEnd() {
 
 function startSounds() {
   document.querySelector("#background_music").play();
-  document.querySelector("#background_music").volume = 0.3;
+  document.querySelector("#background_music").volume = 0.7;
   document.querySelector("#skull_sound").volume = 0.3;
 }
 
@@ -76,9 +145,10 @@ function startSounds() {
 function clickTreasure() {
   console.log("Click");
   let treasure = this;
+  treasure.removeEventListener("mousedown", clickTreasure);
+  console.log(this);
   document.querySelector("#treasure_click").currentTime = 0;
   document.querySelector("#treasure_click").play();
-  treasure.removeEventListener("mousedown", clickTreasure);
   treasure.classList.add("paused");
   incrementPoints();
   treasure.querySelector("img").classList.add("zoom_out");
@@ -93,15 +163,18 @@ function treasureGone() {
   treasure.classList.remove("paused");
   treasure.querySelector("img").classList.remove("zoom_out");
   // genstart jumping animation
-  treasureRestart.call(this);
+  if (isGameRunning) {
+    treasure.addEventListener("mousedown", clickTreasure);
+    treasureRestart.call(this);
+  }
 }
 
 function treasureRestart() {
   let treasure = this;
+  treasure.removeEventListener("animationend", skullRestart);
   treasure.classList.remove("jumping1", "jumping2", "jumping3");
   treasure.offsetWidth;
   randomizedJumping.call(this);
-  // fjern position class, lav ny position nummer, sæt position class på igen.
   treasure.classList.remove(
     "position1",
     "position2",
@@ -113,7 +186,7 @@ function treasureRestart() {
 
   let pos = Math.floor(Math.random() * 6) + 1;
   treasure.classList.add("position" + pos);
-  treasure.addEventListener("click", clickTreasure);
+  treasure.addEventListener("animationend", treasureRestart);
 }
 
 // tilfældig nummer til jumping animation
@@ -128,12 +201,7 @@ function incrementPoints() {
   console.log("Points increment");
   points++;
   // winstate
-  if (points >= 10) {
-    displayPoints();
-    levelComplete();
-  } else {
-    displayPoints();
-  }
+  displayPoints();
 }
 
 function displayPoints() {
@@ -158,10 +226,33 @@ function skullGone() {
   skull.removeEventListener("animationend", skullGone);
   skull.querySelector("img").classList.remove("zoom_in");
   skull.classList.remove("paused");
-  skull.classList.remove("jumping");
+  if (isGameRunning) {
+    skull.classList.remove("jumping");
+    skull.offsetWidth;
+    skull.classList.add("jumping");
+    skull.addEventListener("mousedown", clickSkull);
+  }
+}
+
+function skullRestart() {
+  // fjern event der bringer os herind
+  let skull = this;
+  skull.removeEventListener("animationend", skullRestart);
+  skull.classList.remove("jumping_skull");
   skull.offsetWidth;
-  skull.classList.add("jumping");
-  skull.addEventListener("mousedown", clickSkull);
+  skull.classList.add("jumping_skull");
+  skull.classList.remove(
+    "position1",
+    "position2",
+    "position3",
+    "position4",
+    "position5",
+    "position6"
+  );
+
+  let pos = Math.floor(Math.random() * 6) + 1;
+  skull.classList.add("position" + pos);
+  skull.addEventListener("animationend", skullRestart);
 }
 
 // ======= Click rum Function ======= //
@@ -169,27 +260,28 @@ function clickRum() {
   console.log("click rum");
   let rum = document.querySelector("#rum_container");
   if (lives >= 3) {
-    incrementPoints(); 
     incrementPoints();
-} else {
+    incrementPoints();
+  } else {
     incrementLives;
-}
+  }
   rum.removeEventListener("mousedown", clickRum);
   rum.classList.add("paused");
   rum.querySelector("img").classList.add("zoom_out");
   rum.addEventListener("animationend", rumGone);
-  
 }
 
 function rumGone() {
   let rum = document.querySelector("#rum_container");
   rum.removeEventListener("animationend", rumGone);
   rum.querySelector("img").classList.remove("zoom_out");
-  rum.classList.remove("paused");
-  rum.classList.remove("jumping_rum");
-  rum.offsetWidth;
-  rum.classList.add("jumping_rum");
-  rum.addEventListener("mousedown", clickRum);
+  if (isGameRunning) {
+    rum.classList.remove("paused");
+    rum.classList.remove("jumping_rum");
+    rum.offsetWidth;
+    rum.classList.add("jumping_rum");
+    rum.addEventListener("mousedown", clickRum);
+  }
 }
 
 function rumRestart() {
@@ -226,12 +318,13 @@ function displayIncrementedLives() {
 
 // ======= Decrement lives ======= //
 function decrementLives() {
-  lives--;
-  if (lives === 0) {
+  if (lives === 1) {
+    document.querySelector("#end_text").textContent = `Du løb tør for liv!`;
     gameOver();
   } else {
     displayDecrementedLives();
   }
+  lives--;
 }
 
 function displayDecrementedLives() {
@@ -241,18 +334,100 @@ function displayDecrementedLives() {
 }
 
 function gameOver() {
+  stopGame();
   document.querySelector("#game_over").classList.remove("hidden");
   document.querySelector("#skull_sound").pause();
   document.querySelector("#background_music").pause();
+  document.querySelector("#death_music").currentTime = 0;
   document.querySelector("#death_music").play();
   document.querySelector("#death_music").loop = true;
+  document.querySelector("#crying_man").currentTime = 0;
   document.querySelector("#crying_man").play();
   document.querySelector("#crying_man").loop = true;
   document.querySelector("#crying_man").volume = 0.6;
+  document
+    .querySelector("#restart_game_button")
+    .addEventListener("click", restartGame);
+  document.querySelector(
+    "#highscore_lose"
+  ).textContent = `Highscore: ${highScore}`;
   console.log("GAME OVER.");
 }
 
 function levelComplete() {
+  stopGame();
+  if (highScore < points) {
+    highScore = points;
+  }
+  document.querySelector(
+    "#highscore_win"
+  ).textContent = `Highscore: ${highScore}`;
   console.log("LEVEL COMPLETE!");
   document.querySelector("#level_complete").classList.remove("hidden");
+  document.querySelector("#complete_score").textContent = `Score: ${points}`;
+}
+
+function restartGame() {
+  document.querySelector("#crying_man").pause();
+  document.querySelector("#death_music").pause();
+  document.querySelector("#game_over").classList.add("hidden");
+}
+
+function startTimer() {
+  // Sæt timer-animationen (shrink) i gang ved at tilføje klassen shrink til time_sprite
+  document.querySelector("#time_sprite").classList.remove("shrink");
+  document.querySelector("#time_sprite").offsetWidth;
+  document.querySelector("#time_sprite").classList.add("shrink");
+
+  // Tilføj en eventlistener som lytter efter at animationen er færdig (animationend) og kalder funktionen timeIsUp
+  document
+    .querySelector("#time_sprite")
+    .addEventListener("animationend", timeIsUp);
+}
+
+function timeIsUp() {
+  console.log("Tiden er gået!");
+
+  if (points >= 15) {
+    levelComplete();
+  } else {
+    document.querySelector(
+      "#end_text"
+    ).textContent = `Du fik ikke over 15 point før tiden gik ud.`;
+    gameOver();
+  }
+}
+
+function stopGame() {
+  isGameRunning = false;
+
+  //fjern jumping
+  document
+    .querySelector("#treasure1_container")
+    .classList.remove("jumping1", "jumping2", "jumping3");
+  document
+    .querySelector("#treasure2_container")
+    .classList.remove("jumping1", "jumping2", "jumping3");
+  document
+    .querySelector("#treasure3_container")
+    .classList.remove("jumping1", "jumping2", "jumping3");
+  document.querySelector("#skull_container").classList.remove("jumping_skull");
+  document.querySelector("#rum_container").classList.remove("jumping_rum");
+
+  //fjern event listeners
+  document
+    .querySelector("#treasure1_container")
+    .removeEventListener("mousedown", clickTreasure);
+  document
+    .querySelector("#treasure2_container")
+    .removeEventListener("mousedown", clickTreasure);
+  document
+    .querySelector("#treasure3_container")
+    .removeEventListener("mousedown", clickTreasure);
+  document
+    .querySelector("#skull_container")
+    .removeEventListener("mousedown", clickSkull);
+  document
+    .querySelector("#rum_container")
+    .removeEventListener("mousedown", clickRum);
 }
